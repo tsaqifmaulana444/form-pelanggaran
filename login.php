f6e120a4451817cd5fb30923cbbc20d87728e07e
fauzankirana160526 <kiranafauzan6@gmail.com>
2022-10-04 08:38:54 +0700

    login fauzan

---
+++ login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <!-- iconfity -->
+    <script src="https://code.iconify.design/iconify-icon/1.0.0/iconify-icon.min.js"></script>
+    <style>
+        <?php include 'login.css'?>
+    </style>
+</head>
+<body>
+    <div class="login">
+        <div class="gmail">
+            <p>Gmail</p>
+            <input type="gmail">
+        </div>
+            <p>Login</p>
+        <div class="lgn">
+            <p style="margin-left: -7%;">Hey, enter your details to get sign,</p>
+            <br>
+            <p style="margin-left: -3.9%;">to your account</p>
+        </div>
+        <div class="password">
+            <p>Password</p>
+            <input type="password">
+        </div>
+        <div class="forgot1">
+        <a href=""><p>Forgot password</p></a>
+        </div>
+        <div class="buttom">
+            <a href=""><p>Sign in</p></a>
+        </div>
+        <div class="buttom1">
+            <a href=""><p><iconify-icon icon="flat-color-icons:google"></iconify-icon></p></a>
+            <a href=""><h3>Sign In Gith Google</h3></a>
+        </div>
+</body>
+</html>
